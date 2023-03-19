9bf4929fa729e2bfee16a1c247c5519d35f9d38f
lacky fish <makeykin87@mail.ru>
2023-03-19 12:13:13 +0300

    homework.seminar6

---
+++ task41/Program.cs
@@ -0,0 +1,46 @@
+﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+// 0, 7, 8, -2, -2 -> 2
+
+// 1, -7, 567, 89, 223-> 3
+
+
+int ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToInt32(Console.ReadLine());
+}
+int[] InputArray(int length)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = ReadInt($"введите {i + 1} -й элемент");
+    }
+    return array;
+}
+void PrintArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        System.Console.WriteLine($"a[{i}]={array[i]}");
+    }
+}
+int CountPositiveNumber(int[] array)
+{
+    int count = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > 0)
+        {
+            count++;
+        }
+    }
+    return count;
+}
+int length=ReadInt("введите количество элементов  > " );
+int[]array;
+array= InputArray(length);
+PrintArray(array);
+System.Console.WriteLine($"количество чисел больше 0 - { CountPositiveNumber(array)}");
---
+++ task43/Program.cs
@@ -0,0 +1,58 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых,
+//  заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+const int coeff = 0;
+const int constant = 1;
+const int x_coords = 0;
+const int y_coords = 1;
+const int Line1 = 1;
+const int Line2 = 2;
+
+double[] lineData1 = InputLineData(Line1);
+double[] lineData2 = InputLineData(Line2);
+
+if (ValidateLines(lineData1, lineData1))
+{
+    double[] coord = Findcoords(lineData1, lineData1);
+    System.Console.WriteLine($"точка пересечений уравнений y= {lineData1[coeff]}*x +{lineData1[constant]} и y= {lineData2[coeff]}*x +{lineData2[constant]} ");
+System.Console.WriteLine($"имеет координаты ({coord[x_coords]},{coord[y_coords]})");
+}
+
+double ReadInt(string text)
+{
+    System.Console.WriteLine(text);
+    return Convert.ToDouble(Console.ReadLine());
+}
+
+double[] InputLineData(int numberofline)
+{
+    double[] lineData = new double[2];
+    lineData[coeff] = ReadInt($"введите коеффициент для {numberofline} прямой > ");
+    lineData[constant] = ReadInt($"введите константу для {numberofline} прямой > ");
+    return lineData;
+}
+double[] Findcoords(double[] lineDa1, double[] lineData2)
+{
+    double[] coord = new double[2];
+    coord[x_coords] = (lineData1[constant] - lineData2[constant]) / (lineData1[coeff] - lineData2[coeff]);
+    coord[y_coords] = (lineData1[constant] * coord[x_coords] + lineData1[constant]);
+    return coord;
+}
+bool ValidateLines(double[] lineDa1, double[] lineData2)
+{
+    if (lineData1[coeff] == lineData2[coeff])
+    {
+        if (lineData1[constant] == lineData2[constant])
+        {
+            System.Console.WriteLine("прямые совпадают");
+            return false;
+        }
+        else
+        {
+            System.Console.WriteLine("прямые параллельны");
+            return false;
+        }
+    }
+    return true;
+}
